de53b4eb91043c0177f70fd71000533f43941ac4
QuenMBar <Qmb2@students.calvin.edu>
2018-10-19 13:38:46 -0400

    Working Timer, more functionality to come

--- app/src/main/java/edu/calvin/cs262/teama/timetracker/MainActivity.java
+++ app/src/main/java/edu/calvin/cs262/teama/timetracker/MainActivity.java
@@ -2,6 +2,8 @@ package edu.calvin.cs262.teama.timetracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,10 +16,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.weonseok.timetracker.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,37 +28,19 @@ public class MainActivity extends AppCompatActivity
     Spinner spinActivities;
     String[] activitiesList = {"Gaming", "Studying", "Socializing", "Partying"};
 
+    int seconds;
+    boolean running;
+    boolean clicked;
+    ImageView playPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "COMING SOON!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        spinActivities = (Spinner)findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, activitiesList);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinActivities.setAdapter(adapter);
+        startSpinner();
+        clicked = false;
+        playPause = (ImageView)findViewById(R.id.play);
+        runTimer();
     }
 
     @Override
@@ -115,17 +100,93 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    public void startSpinner() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "COMING SOON!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        spinActivities = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, activitiesList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinActivities.setAdapter(adapter);
+    }
+
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
     }
 
     public void showStartMsg(View view){
+        startTimer(null);
         showMsg("Start time!");
     }
+
     public void showMsg(String message) {
         displayToast(message);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void startTimer(View view){
+        if (!clicked) {
+            playPause.setImageResource(R.drawable.start);
+            clicked = true;
+            running = true;
+        } else if (clicked){
+            playPause.setImageResource(R.drawable.play);
+            clicked = false;
+            running = false;
+        } else {
+            clicked = false;
+            running = false;
+            seconds = 0;
+        }
+    }
+
+    public void stopTimer(View view){
+        running = false;
+    }
+
+    public void resetTimer(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    public void runTimer(){
+        final TextView timerText = (TextView)findViewById(R.id.timerText);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int sec = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, sec);
+                timerText.setText(time);
+                if(running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
 }
+
