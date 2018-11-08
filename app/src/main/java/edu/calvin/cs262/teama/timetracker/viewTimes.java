9813e567589402cb61dbdbc4cfbfcc9ff91f67ec
QuenMBar <Qmb2@students.calvin.edu>
2018-11-08 15:58:41 -0500

    Prototype graph added, I still need to merge it manually

--- app/src/main/java/edu/calvin/cs262/teama/timetracker/viewTimes.java
+++ app/src/main/java/edu/calvin/cs262/teama/timetracker/viewTimes.java
@@ -7,7 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class viewTimes extends AppCompatActivity {
+
+    float timesSpentOnProjects[] = {20, 50, 40, 80};
+    String projectNames[] = {"Project Alpha", "Project Beta", "Project Gamma", "Project Zeta"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +28,26 @@ public class viewTimes extends AppCompatActivity {
         setContentView(R.layout.activity_view_times);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setUpPieChart();
+    }
+
+    private void setUpPieChart() {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for(int i = 0; i < timesSpentOnProjects.length; i++) {
+            pieEntries.add(new PieEntry(timesSpentOnProjects[i], projectNames[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Time Spent on Projects");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+        PieChart chart = (PieChart)findViewById(R.id.viewTimesChart);
+        chart.setData(data);
+        Legend legend = chart.getLegend();
+        legend.setWordWrapEnabled(true);
+        chart.animateY(1000);
+        chart.invalidate();
     }
 
 }
